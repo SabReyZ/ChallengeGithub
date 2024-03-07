6d698b64732c109ab62a0c32c4d0dc8be81092b5
sabrina <sabrina@rey.fr>
2024-03-07 11:04:10 +0100

    Create JS files and add a list on the console

---
+++ fileJStd.js
@@ -0,0 +1,6 @@
+let legumes = ["ail","batterave","champignon","courgette","haricot","poivron","roquette","mais","patate","radis","oignon"];
+console.log(legumes);
+let first = legumes[0];
+console.log(first)
+let last = legumes[legumes.length - 1];
+console.log(last)
